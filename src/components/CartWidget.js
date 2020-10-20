21e2d9da2a29a1315363438e7e07c1777f66ab68
RoxanaHernandez <roxanahernandez326@gmail.com>
2020-10-20 19:01:45 -0300

    'Pryecto-final'

---
+++ src/components/CartWidget.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'materialize-css';
+import { Icon } from 'react-materialize';
+
+function CartWidget(){
+    return <Icon>shopping_cart</Icon>
+}
+
+export default CartWidget;
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CartWidget from '../CartWidget';
+
+function NavBar(){
+    return <div><CartWidget></CartWidget>
+    
+    </div>
+    
+}
+
+export default NavBar;
