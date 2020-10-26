3ef8e3f1aca3b1d7663071aab2439be8f5b79450
RoxanaHernandez <roxanahernandez326@gmail.com>
2020-10-26 20:02:36 -0300

    'originales'

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Home  from './components/Home';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/botones/ItemCount';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Home greeting="Welcome to React">Coders</Home>
       <Home greeting="Bienvenidos a React"></Home>
       <ItemListContainer title="My E-Commerce"></ItemListContainer>
+      <ItemCount initial="" min="1" max="5" onAdd="">1</ItemCount>
+      
     </div>
   );
 }
