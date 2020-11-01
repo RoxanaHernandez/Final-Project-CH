7ba9e5f657187c6284748c06b1e2aceefe454760
RoxanaHernandez <roxanahernandez326@gmail.com>
2020-11-01 18:59:39 -0300

    'itc'

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
-import Home  from './components/Home';
 import ItemListContainer from './components/ItemListContainer';
 import ItemCount from './components/botones/ItemCount';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+ 
   return (
     <div className="App">
       <NavBar></NavBar>
-      <Home greeting="Welcome to React">Coders</Home>
-      <Home greeting="Bienvenidos a React"></Home>
-      <ItemListContainer title="My E-Commerce"></ItemListContainer>
+      
+      <ItemListContainer title= "PRODUCTS"/>
       <ItemCount stock={4} initial={0} onAdd={console.log()}/>
       
     </div>
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,12 +1,33 @@
-import React from 'react';
-function ItemListContainer(props){
+import React, {useState, useEffect} from 'react';
 
-    return (
-      <div>
-        <h1>{props.title}</h1>
-        <div style={{color:"blue", fontSize:90 }}> </div>
-      </div>
-    );
+
+
+
+
+function ItemListContainer(){
+  const APP_ID = "70b628bb";
+  const APP_KEY = " 976a3af59448c1e21bef96e5ec4ce09b";
+  const [recipes, setRecipes]= useState([]);
+  
+  useEffect(()=>{
+    getRecipes();
+  },[]);
+  const getRecipes = async ()=> {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data =  await response.json();
+    setRecipes(data.hits);
   }
+
+
+return (
+  <div className = 'tiendita'>
+    <form className = 'search-form'>
+      <input classsName ='search-bar' type='text'/>
+      <button className = 'search-button' type = 'submit'>SEARCH</button>
+    </form>
+
+  </div>
+)
+};
 
 export default ItemListContainer;
